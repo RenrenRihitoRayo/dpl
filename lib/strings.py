daffc31d5de2691f0b3d3971ec4dd655d6089a54
Darren Papa <darrenchasepapa@gmai.com>
2024-11-25 04:02:24 +0800

    1.4.0 :D

--- lib/strings.py
+++ lib/strings.py
@@ -10,3 +10,24 @@ ext = dpl.extension("string")
 @ext.add_func()
 def split(_, __, string, delim=" ", times=-1):
     return string.split(delim, times)
+
+@ext.add_func()
+def lower(_, __, string):
+    return string.lower()
+
+@ext.add_func()
+def upper(_, __, string):
+    return string.upper()
+
+@ext.add_func()
+def isLower():
+    return string.islower()
+
+@ext.add_func()
+def isUpper():
+    return string.isupper()
+
+@ext.add_func("myHash")
+def myHash(_, __, string):
+    "THIS IS NOT A PERFECT HASH FUNCTION AND JUST USES PYTHONS HASH FUNC\nTHIS WILL BE REPLACED WITH sha256 SOON"
+    return hash(string)
