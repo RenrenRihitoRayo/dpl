e64d1ab6ff17000906277621b481fe0342537bfe
DarrenPapa <darrenchasepapa@gmail.com>
2025-02-09 22:06:28 +0800

    1.4.4

--- git-push.sh
+++ git-push.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ "$#" -ne 1 ]]; then
-    echo ": git-push.sh [message]"
+if [[ "$#" -ne 2 ]]; then
+    echo ": git-push.sh [branch] [message]"
     exit 1
 fi
 
 
 cp ./docs/updates.md README.md
 git add .
-git commit -m "$1"
-git push origin master --force # scary (0o0)
+git commit -m "$2"
+git push origin "$1" --force # scary (0o0)
