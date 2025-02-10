e1f2fe85c189f6b1510fd50f96c58f489eb06563
DarrenPapa <darrenchasepapa@gmail.com>
2025-02-10 21:36:25 +0800

    Updated bash scripts

--- git-push.sh
+++ git-push.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ "$#" -ne 2 ]]; then
-    echo ": git-push.sh [branch] [message]"
+if [[ "$#" -ne 1 ]]; then
+    echo ": git-push.sh [message]"
     exit 1
 fi
 
 
 cp ./docs/updates.md README.md
 git add .
-git commit -m "$2"
+git commit -m "$1"
 git push origin
