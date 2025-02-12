7d5c858899f540593a4084207405f17df844da02
Darren Papa <darrenchasepapa@gmai.com>
2025-02-12 14:17:21 +0800

    Updated new mechanism to install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,40 @@
-
-if [ -z "$ROOT" ]; then
-    echo "\$ROOT not defined!"
-    exit 1
-fi
-
 if [ "$1" = "remove" ]; then
     rm "$ROOT/bin/dpl"
     exit
 fi
 
-# add priv
-echo "Adding privileges..."
-chmod +x ./dpl.sh
-# link file
-echo "Making a symbolic link..."
-ln -sf ~/dpl/dpl.sh "$ROOT/bin/dpl"
+if [ "$1" = "abs_root" ]; then
+    # add priv
+    echo "Adding privileges..."
+    chmod +x ./dpl.sh
+    # link file
+    echo "Making a symbolic link..."
+    ln -sf ~/dpl/dpl.sh "/bin/dpl"
 
-echo "Checking link"
-if command -v dpl >/dev/null 2>&1; then
-    echo "Success!"
+    echo "Checking link"
+    if command -v dpl >/dev/null 2>&1; then
+        echo "Success!"
+    else
+        echo "Failed to detect the linked file."
+        exit 1
+    fi
 else
-    echo "Failed to detect the linked file."
-    exit 1
+    if [ -z "$ROOT" ]; then
+        echo "\$ROOT not defined!"
+        exit 1
+    fi
+    # add priv
+    echo "Adding privileges..."
+    chmod +x ./dpl.sh
+    # link file
+    echo "Making a symbolic link..."
+    ln -sf ~/dpl/dpl.sh "$ROOT/bin/dpl"
+
+    echo "Checking link"
+    if command -v dpl >/dev/null 2>&1; then
+        echo "Success!"
+    else
+        echo "Failed to detect the linked file."
+        exit 1
+    fi
 fi
