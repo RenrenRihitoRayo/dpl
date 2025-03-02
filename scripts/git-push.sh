bfd9561e20ce8e7749b398493ee5020580800f05
DarrenPapa <darrenchasepapa@gmail.com>
2025-03-02 19:00:50 +0800

    fixed bugs

--- scripts/git-push.sh
+++ scripts/git-push.sh
@@ -13,11 +13,11 @@ git add .
 git commit -m "$2"
 git push origin $1 --force
 
-echo "[  git-push.sh  ]: Updating master..."
+echo "\n[  git-push.sh  ]: Updating master..."
 git checkout master
 git merge origin $1
 git add .
 git commit -m "Automatic merge from $1"
 git push origin master --force
 git checkout $1
-echo "[  git-push.sh  ]: Done!"
+echo "\n[  git-push.sh  ]: Done!"
