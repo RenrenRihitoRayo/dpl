6396b22bb0c2c8bbeeeeadd0f1db1314f1273f9e
DarrenPapa <darrenchasepapa@gmail.com>
2025-03-02 19:01:08 +0800

    fixed bugs

--- scripts/git-push.sh
+++ scripts/git-push.sh
@@ -8,16 +8,16 @@ elif [[ "$1" == "help" ]]; then
     exit 0
 fi
 
-echo "[  git-push.sh  ]: Pushing $1..."
+echo -e "[  git-push.sh  ]: Pushing $1..."
 git add .
 git commit -m "$2"
 git push origin $1 --force
 
-echo "\n[  git-push.sh  ]: Updating master..."
+echo -e "\n[  git-push.sh  ]: Updating master..."
 git checkout master
 git merge origin $1
 git add .
 git commit -m "Automatic merge from $1"
 git push origin master --force
 git checkout $1
-echo "\n[  git-push.sh  ]: Done!"
+echo -e "\n[  git-push.sh  ]: Done!"
