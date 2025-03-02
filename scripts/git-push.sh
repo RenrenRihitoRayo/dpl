b641d97dfc85d66c5000d0fa9594b4b5e84091b2
DarrenPapa <darrenchasepapa@gmail.com>
2025-03-02 18:41:39 +0800

    fixed bugs

--- scripts/git-push.sh
+++ scripts/git-push.sh
@@ -5,6 +5,14 @@ if [[ "$#" -ne 2 ]]; then
     exit 1
 fi
 
+echo "[git-push.sh]: Pushing $1..."
 git add .
 git commit -m "$2"
 git push origin $1 --force
+
+echo "[git-push.sh]: Updating master..."
+git checkout master
+git add .
+git commit -m "Automatic merge from $1"
+git push origin master --force
+git checkout $1
