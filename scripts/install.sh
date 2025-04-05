1b2ac5a64e4c257fe1f9d454af46d7ebef794a2d
DarrenPapa <darrenchasepapa@gmail.com>
2025-04-05 19:56:41 +0800

    Updated install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,10 @@
 local_dir="$(pwd)"
 
+echo "dpl.py run $@" > dpl-run
+chmod +x dpl-run
+echo "dpl.py $@" > dpl
+chmod +x dpl
+
 # Check if it's already in .bashrc
 if ! grep -Fxq "export PATH=\"$local_dir:\$PATH\"" ~/.bashrc; then
     echo "export PATH=\"$local_dir:\$PATH\"" >> ~/.bashrc
