628a9762c00c3743d4d451df824b5fbc7382494e
DarrenPapa <darrenchasepapa@gmail.com>
2025-04-05 19:50:46 +0800

    Udated install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,32 +1,9 @@
 local_dir="$(pwd)"
 
-dpl_file="$(pwd)/dpl.py"
-
-echo "$dpl_file run \"\$@\"" > dpl-run.sh
-echo "$dpl_file \"\$@\"" > dpl.sh
-
-chmod +x $local_dir/dpl.py
-chmod +x $local_dir/dpl.sh
-chmod +x $local_dir/dpl-run.sh
-
-if [ "$1" = "help" ]; then
-    cat << 'EOF'
-DPL Installation Tool [DIT] 1.0
-
-install help     - print this
-
-Privileges are required for the following commands.
-install in [path] - Installs the program in that path.
-EOF
-    exit
-fi
-
-if [ "$1" = "in" ]; then
-    install_path=$2
-    echo "Installing in $install_path"
-    
-    echo "Linking..."
-    ln $install_path/dpl $local_dir/dpl.sh
-    ln $install_path/dpl-run $local_dir/dpl-run.sh
-    echo "Done!"
+# Check if it's already in .bashrc
+if ! grep -Fxq "export PATH=\"$local_dir:\$PATH\"" ~/.bashrc; then
+    echo "export PATH=\"$local_dir:\$PATH\"" >> ~/.bashrc
+    echo "Added $local_dir to PATH in ~/.bashrc"
+else
+    echo "$local_dir already in PATH in ~/.bashrc"
 fi
