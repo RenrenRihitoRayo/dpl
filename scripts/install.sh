787ba5a86a0db017a9034f9196df60a17c80a841
DarrenPapa <darrenchasepapa@gmail.com>
2025-04-05 19:30:52 +0800

    Added more info in README

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,6 +6,8 @@ echo "$dpl_file run \"\$@\"" > dpl-run.sh
 echo "$dpl_file \"\$@\"" > dpl.sh
 
 chmod +x $local_dir/dpl.py
+chmod +x $local_dir/dpl.sh
+chmod +x $local_dir/dpl-run.sh
 
 if [ "$1" = "help" ]; then
     cat << 'EOF'
@@ -14,88 +16,17 @@ DPL Installation Tool [DIT] 1.0
 install help     - print this
 
 Privileges are required for the following commands.
-install          - ROOT must be defined
-install remove   - Remove the generated symlink
-install abs_root - It install it in the root
+install in [path] - Installs the program in that path.
 EOF
     exit
 fi
 
-if [ "$1" = "remove" ]; then
-    rm "$ROOT/bin/dpl"
-    rm "$ROOT/bin/dpl-run"
-    exit
-fi
-
-if [ "$1" = "remove-ar" ]; then
-    rm "/bin/dpl"
-    rm "/bin/dpl-run"
-    exit
-fi
-
-if [ "$1" = "abs_root" ]; then
-    ROOT=$2
-    # add priv
-    echo "dpl.sh: Adding privileges..."
-    chmod +x $local_dir/dpl.sh
-    echo "dpl-run.sh: Adding privileges..."
-    chmod +x $local_dir/dpl-run.sh
-    # link file - dpl.sh
-    echo "dpl.sh: Making a symbolic link..."
-    ln -sf $local_dir/dpl.sh "$ROOT/bin/dpl"
-    chmod +x "$local_dir/dpl.sh"
-
-    echo "Checking link: dpl"
-    if command -v dpl >/dev/null 2>&1; then
-        echo "dpl: Success!"
-    else
-        echo "Failed to detect the linked file."
-        exit 1
-    fi
+if [ "$1" = "in" ]; then
+    install_path=$2
+    echo "Installing in $install_path"
     
-    # link file - dpl-run.sh
-    echo "dpl-run.sh: Making a symbolic link..."
-    ln -sf $local_dir/dpl-run.sh "$ROOT/bin/dpl-run"
-    chmod +x "$local_dir/dpl-run.sh"
-
-    echo "Checking link: dpl-run"
-    if command -v dpl-run >/dev/null 2>&1; then
-        echo "dpl-run: Success!"
-    else
-        echo "Failed to detect the linked file."
-        exit 1
-    fi
-else
-    if [ -z "$ROOT" ]; then
-        echo "\$ROOT not defined!"
-        exit 1
-    fi
-    # add priv
-    echo "dpl.sh: Adding privileges..."
-    chmod +x ./dpl.sh
-    echo "dpl-run.sh: Adding privileges..."
-    chmod +x ./dpl-run.sh
-    # link file - dpl.sh
-    echo "dpl.sh: Making a symbolic link..."
-    ln -sf $local_dir/dpl.sh "$ROOT/bin/dpl"
-
-    echo "Checking link: dpl"
-    if command -v dpl >/dev/null 2>&1; then
-        echo "dpl: Success!"
-    else
-        echo "Failed to detect the linked file."
-        exit 1
-    fi
-    
-    # link file - dpl-run.sh
-    echo "dpl-run.sh: Making a symbolic link..."
-    ln -sf $local_dir/dpl-run.sh "$ROOT/bin/dpl-run"
-
-    echo "Checking link: dpl-run"
-    if command -v dpl-run >/dev/null 2>&1; then
-        echo "dpl-run: Success!"
-    else
-        echo "Failed to detect the linked file."
-        exit 1
-    fi
+    echo "Linking..."
+    ln $install_path/dpl $local_dir/dpl.sh
+    ln $install_path/dpl-run $local_dir/dpl-run.sh
+    echo "Done!"
 fi
